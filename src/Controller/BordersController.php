c3683c02cd492c33841a83e3fa94c0eb1621d337
r-takagi <negro8971@gmail.com>
2020-02-18 03:30:54 +0900

    ADD レコード追加処理

--- src/Controller/BordersController.php
+++ src/Controller/BordersController.php
@@ -5,5 +5,14 @@ class BordersController extends AppController {
     public function index() {
         $data = $this->Borders->find('all');
         $this->set('data',$data);
+        $this->set('entity',$this->Borders->newEntity());
+    }
+
+    public function addRecord(){
+        if ($this->request->is('post')) {
+            $bord = $this->Borders->newEntity($this->request->data);
+            $this->Bords->save($board);
+        }
+        return $this->redirect(['action' => 'index']);
     }
 }
--- src/Template/Borders/index.ctp
+++ src/Template/Borders/index.ctp
@@ -1,3 +1,14 @@
+<h1>DBレコード追加サンプル</h1>
+<?=$this->Form->create($entity,['url'=>['action'=>'addRecord']]) ?>
+<fieldset>
+<?=$this->Form->text("name") ?>
+<?=$this->Form->text("title") ?>
+<?=$this->Form->textarea("content") ?>
+</fieldset>
+<?=$this->Form->button("送信") ?>
+<?=$this->Form->end() ?>
+
+<hr>
 
 <table>
 <?php foreach ($data as $obj): ?>
